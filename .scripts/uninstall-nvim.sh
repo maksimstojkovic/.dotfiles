970a52f9939fa7f3d62d26e068bb41f7004389da
Maksim Stojkovic <18454392+silentdigit@users.noreply.github.com>
2019-07-06 02:00:50 +0200

    Updated uninstall TODO

--- .scripts/uninstall-nvim.sh
+++ .scripts/uninstall-nvim.sh
@@ -7,6 +7,7 @@ stow --verbose=2 -D nvim
 
 cd tinytex/bin
 stow --verbose=2 -D -t /usr/local/bin/ x86_64-linux/
+# TODO run R command for uninstalling, with same directory
 
 cd /usr/local/stow
 rm -rf nvim tinytex
