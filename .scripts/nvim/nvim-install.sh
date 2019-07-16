c3db1c082e9cf1abf5ad510cd360d9b393f1e038
Maksim Stojkovic <18454392+silentdigit@users.noreply.github.com>
2019-07-16 23:01:41 +0200

    NVIM script functional

--- .scripts/nvim/nvim-install.sh
+++ .scripts/nvim/nvim-install.sh
@@ -30,42 +30,78 @@ if [ "$dir" == "${dir/${home}\/.dotfiles/}" ]; then
 	exit 1
 fi
 
-echo "INSTALLING DEPENDENCIES"
-# 	apt-get remove -y neovim
-# 	apt-get install -y stow build-essential
-# 	apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
-# 	apt-get install -y python-dev python-pip python3-dev python3-pip
-# 	sudo -u ${user} pip install -q --user pynvim
-echo "INSTALLING DEPENDENCIES DONE"
+# echo "INSTALLING DEPENDENCIES"
+# apt-get remove -y neovim
+# apt-get install -y stow build-essential
+# apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+# apt-get install -y python-dev python-pip python3-dev python3-pip
+# apt-get install -y r-base pandoc pandoc-citeproc
+# sudo -u ${user} pip install -q --user pynvim
+# echo "INSTALLING DEPENDENCIES DONE"
 
 # helper for unstowing packages if they exist
 unstow() {
-	package=$(basename $1)
-	if [ -d "${stow_dir}/${package}" ]; then
-		cd $stow_dir
-		stow --verbose=2 -D $package
-	fi
+package=$(basename $1)
+if [ -d "${stow_dir}/${package}" ]; then
+	cd $stow_dir
+	stow --verbose=2 -D $package
+fi
 }
 
 echo "REMOVING REQUIRED DIRECTORIES"
 unstow nvim
 unstow tinytex
-rm -rf /tmp/nvim /tmp/fonts $nvim_stow $tinytex_opt $tinytex_stow
+rm -v -rf /tmp/nvim /tmp/fonts $nvim_stow $tinytex_opt $tinytex_stow
 cd /usr/local
-find -type f -name '*nvim*' -delete
+find -type f -name "*nvim*" -delete
 echo "REMOVING REQUIRED DIRECTORIES DONE"
 
 echo "INSTALLING NEOVIM FROM SOURCE"
-#	sudo -u ${user} git clone https://github.com/neovim/neovim /tmp/nvim
-	sudo -u $user cp -r -v /tmp/vim /tmp/nvim
-	cd /tmp/nvim
-	rm -rf build
-	sudo -u $user make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=${nvim_stow}
-	make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=${nvim_stow} install
-	cd $stow_dir
-	stow --verbose=2 nvim
-	update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
+# sudo -u ${user} git clone https://github.com/neovim/neovim /tmp/nvim
+sudo -u $user cp -r -v /tmp/vim /tmp/nvim
+cd /tmp/nvim
+rm -rf build
+sudo -u $user make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=${nvim_stow}
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=${nvim_stow} install
+cd $stow_dir
+stow --verbose=2 nvim
+update-alternatives --install /usr/bin/vim vim /usr/local/bin/nvim 60
 echo "INSTALLING NEOVIM FROM SOURCE DONE"
 
+echo "SYMLINKING .vimrc AND init.vim FILES"
+cd ${home}/.dotfiles
+stow --verbose=2 vim
+echo "SYMLINKING .vimrc AND init.vim FILES DONE"
 
+echo "INSTALLING VIM PLUGINS"
+vim +"PlugInstall | q! | q!" ~/$RANDOM.txt --headless
+echo "INSTALLING VIM PLUGINS DONE"
 
+echo "SETTING UP VIM R-MARKDOWN"
+R --no-save << EOF
+install.packages("tinytex")
+tinytex::install_tinytex(dir = "${tinytex_opt}")
+EOF
+
+find ${home}/bin -lname "${tinytex_opt}/*" -delete
+rm -v -d ${home}/bin
+mkdir -p ${tinytex_stow}/bin
+ln -v -s ${tinytex_opt}/bin/x86_64-linux/* ${tinytex_stow}/bin
+cd ${stow_dir}
+stow --verbose=2 tinytex
+echo "SETTING UP VIM R-MARKDOWN DONE"
+
+# On Windows, install all of the source code pro fonts from https://github.com/powerline/fonts/tree/master/SourceCodePro
+# Windows - Change terminal font to Source Code Pro for Powerline
+# Additional instructions can be found at https://github.com/vim-airline/vim-airline/wiki/Dummies-Guide-to-the-status-bar-symbols-(Powerline-fonts)-on-Fedora,-Ubuntu-and-Windows
+# echo "INSTALLING AIRLINE/POWERLINE PATCHED FONTS"
+# git clone https://github.com/powerline/fonts.git /tmp/fonts
+# cd /tmp/fonts
+# ./install.sh
+# echo "INSTALLING AIRLINE/POWERLINE PATCHED FONTS DONE"
+
+echo
+echo "INSTALLATION COMPLETE"
+echo "NEOVIM SOURCE FILES CAN BE FOUND IN /tmp/nvim"
+echo "ENSURE THAT THE TERMINAL FONT IS SET TO Source Code Pro for Powerline"
+echo "FONT FILES CAN BE FOUND IN /tmp/fonts"
